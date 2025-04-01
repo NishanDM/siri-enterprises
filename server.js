2964805ce543d7c06730c36932b1478f940cdfc6
Nishan Dhananjaya Morathota <nishandm97@gmail.com>
2025-04-01 09:57:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {})
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("MongoDB Connection Error:", err));
 
 // Mongoose Schema
 const OrderSchema = new mongoose.Schema({
