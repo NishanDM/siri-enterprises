7a80f4a83ed6bec7e879035734ec24e9901fb569
Nishan Dhananjaya Morathota <nishandm97@gmail.com>
2025-04-01 11:07:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.post("/order", async (req, res) => {
 });
 
 // Start Server
-// const PORT = process.env.PORT || 4000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 //---------------------THE END-------------------------------------------------//
 
